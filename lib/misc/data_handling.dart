c60f6b2809527f7f7970fdfbe6736ad1963713b7
Saurav Kumar <sauravk7077@gmail.com>
2021-02-01 09:45:00 +0530

    added data handling

---
+++ lib/misc/data_handling.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+void initializeDB() async {
+  Directory appDir = await getApplicationDocumentsDirectory();
+  Hive.init(appDir.path);
+}
+
+Future<void> saveToBox(String boxName, int value) async {
+  Box dateBox = await Hive.openBox(boxName);
+  dateBox.put(DateTime.now(), value);
+}
+
+Future<ValueListenable<Box<dynamic>>> getBoxValueListeneable(
+    String boxName) async {
+  Box dateBox = await Hive.openBox(boxName);
+  return dateBox.listenable();
+}
